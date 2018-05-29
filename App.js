80427008122b31c42fa8b7996942923e094b0f91
shibsho <shibsho@gmail.com>
2018-05-29 09:24:56 +0900

    Post new Memo from TextInput

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
+import {FlatList, StyleSheet, Text, TextInput, View, TouchableOpacity} from 'react-native';
 
-export default class FetchExample extends React.Component {
-
-  constructor(props){
+export default class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state ={ isLoading: true}
+    this.state = {
+      memo_title: "",
+      memo_text: "",
+    };
   }
 
-  componentDidMount(){
-    return fetch('http://localhost:8000/api/users/')
+  componentDidMount() {
+    return fetch('http://localhost:8000/api/memos/')
       .then((response) => response.json())
       .then((responseJson) => {
 
         this.setState({
-          isLoading: false,
           dataSource: responseJson,
         }, function(){
 
@@ -26,26 +27,70 @@ export default class FetchExample extends React.Component {
       });
   }
 
+  post_memo(){
+    fetch('http://localhost:8000/api/memos/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: this.state.memo_title,
+        text: this.state.memo_text,
+      }),
+    });
+  }
 
-
-  render(){
-
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
+  render() {
+    return (
+      
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Memo一覧</Text>
         </View>
-      )
-    }
-
-    return(
-      <View style={{flex: 1, paddingTop:20}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="メモのタイトル"
+          onChangeText={(memo_title) => this.setState({memo_title})}
+        />
+        <TextInput
+          style={{height: 40}}
+          placeholder="メモのテキスト"
+          onChangeText={(memo_text) => this.setState({memo_text})}
+        />
+        <Text>タイトル：{this.state.memo_title}</Text>
+        <Text>テキスト：{this.state.memo_text}</Text>
+      
+        <TouchableOpacity onPress={this.post_memo.bind(this)}>
+          <Text>メモを保存</Text>
+        </TouchableOpacity>
         <FlatList
           data={this.state.dataSource}
-          renderItem={({item}) => <Text>{item.username}</Text>}
-          keyExtractor={(item, index) => index}
+          renderItem={({item}) => <Text>{item.id}{item.title}</Text>}
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    marginTop: 30,
+    padding: 10,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    alignItems: 'center',
+    backgroundColor: '#c6e6fa',
+  },
+  body: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
